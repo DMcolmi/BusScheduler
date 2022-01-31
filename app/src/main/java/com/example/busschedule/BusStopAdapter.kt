50f1d394fe95f82e029d618be86eabe8a4fc2d03
davide <davidemaria.colmi@gmail.com>
2022-01-31 21:18:58 +0100

    start implementing list adapter

---
+++ app/src/main/java/com/example/busschedule/BusStopAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.busschedule
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.busschedule.database.Schedule
+import com.example.busschedule.databinding.ActivityMainBinding
+import com.example.busschedule.databinding.BusStopItemBinding
+import java.text.SimpleDateFormat
+import java.util.*
+
+class BusStopAdapter(private val onItemClicked: (Schedule) -> Unit) : ListAdapter<Schedule, BusStopAdapter.BusStopViewHolder>(DiffCallback) {
+
+    class BusStopViewHolder(private var binding: BusStopItemBinding): RecyclerView.ViewHolder(binding.root){
+
+        @SuppressLint("SimpleDateFormat")
+        fun bind(schedule: Schedule) {
+            binding.stopNameTextView.text = schedule.stopName
+            binding.arrivalTimeTextView.text = SimpleDateFormat(
+                "h:mm a"
+            ).format(
+                Date(schedule.arrivalTime.toLong() * 1000)
+            )
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BusStopViewHolder {
+        val binding = BusStopItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val viewHolder = BusStopViewHolder(binding)
+        viewHolder.itemView.setOnClickListener {
+           val position = viewHolder.adapterPosition
+            onItemClicked(getItem(position))
+        }
+        return viewHolder
+    }
+
+    override fun onBindViewHolder(holder: BusStopViewHolder, position: Int) {
+        val schedule: Schedule= getItem(position)
+        holder.bind(schedule)
+    }
+}
---
+++ app/src/main/java/com/example/busschedule/database/BusScheduleApplication.kt
@@ -0,0 +1,9 @@
+package com.example.busschedule.database
+
+import android.app.Application
+
+class BusScheduleApplication: Application() {
+    val database:AppDatabase by lazy {
+        AppDatabase.getDatabase(this)
+    }
+}
